d2111c7545422d55c886d4f01df08ca2f8538983
rigonhodza <rigon.hoxha10@gmail.com>
2026-03-05 17:46:00 +0100

    gerge

---
+++ moduli26/main.js
@@ -0,0 +1,63 @@
+console.log('hello edlira')
+
+var x = 10;
+
+var value = 'digital schoool';
+
+var y =30
+
+
+console.log(x-y);
+
+console.log(x/y);
+
+console.log(x*y);
+
+console.log(x+y);
+
+var a = 99;
+
+a++;
+
+console.log(a);
+
+var b = 99;
+
+b--;
+
+console.log(b);
+
+var c =450;
+
+c+=50;
+
+console.log(c);
+
+var d =450;
+
+d-=50;
+
+console.log(d);
+
+var e =30;
+
+e*=4;
+
+console.log(e);
+
+var v =100;
+
+var w =200;
+
+console.log(v == w)
+console.log(v != w)
+console.log(v > w)
+console.log(v < w)
+
+var emri = "Rigon"
+var mbiemri = "Hodza"
+
+var fullName = emri+" "+mbiemri;
+console.log(fullName);
+
+console.log('hello from ${emri}');
