4061b7f83ec68731308d1193a98b99f093293a40
rigonhodza <rigon.hoxha10@gmail.com>
2026-03-10 17:45:31 +0100

    moduli27

---
+++ moduli27/main.js
@@ -0,0 +1,11 @@
+function displayalert(){
+    alert('this is inside the function')
+}
+
+displayalert();
+
+function mbledhja(numri1,numri2){
+    return numri1+numri2;
+}
+
+document.write(mbledhja(5,8));
