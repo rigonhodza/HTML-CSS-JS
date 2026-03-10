741dc0743c5142c279ba6bfba68d9fc42f4e456c
rigonhodza <rigon.hoxha10@gmail.com>
2026-03-10 17:20:32 +0100

    kign

--- moduli26/main.js
+++ moduli26/main.js
@@ -61,3 +61,22 @@ var fullName = emri+" "+mbiemri;
 console.log(fullName);
 
 console.log('hello from ${emri}');
+
+var email = "Rigon.hoxha10@gmail.com"
+var passswoerd = "babushi232"
+
+var fullName = emri+" "+mbiemri;
+
+var c =24;
+
+var d =66;
+
+console.log(c>=18 && c<66)
+
+var1 =5;
+
+console.log(var1<=4 || var1>66)
+
+var var2 =10;
+var var3 =12;
+
