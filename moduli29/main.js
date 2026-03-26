d64b3b76b4e968592850b517cd2a48c57d14543a
rigonhodza <rigon.hoxha10@gmail.com>
2026-03-26 17:19:00 +0100

    moduli29

---
+++ moduli29/main.js
@@ -0,0 +1,95 @@
+var propgrammingLanguages = ["Javascript", "PHP", "Python"];
+
+
+console.log(propgrammingLanguages);
+
+
+var numrat = [1,2,3,4,5,6,7,8,9,10];
+
+
+console.log(numrat);
+
+
+console.log(propgrammingLanguages[1]);
+
+
+// array function
+
+
+//push add an elemente at the end of the list
+
+
+propgrammingLanguages.push("Java");
+
+
+numrat.push(11);
+
+
+document.write(propgrammingLanguages);
+
+
+document.write(numrat);
+
+
+//pop delete the last element
+
+
+numrat.pop();
+
+
+console.log(numrat);
+
+
+//unshift - add an element at the begining of the list
+
+
+propgrammingLanguages.unshift("C#");
+
+
+console.log(propgrammingLanguages);
+
+
+// shift - delete the first element of the list 
+
+
+propgrammingLanguages.shift();
+
+
+console.log(propgrammingLanguages);
+
+// splice
+
+propgrammingLanguages.splice [1, 2, 'Ruby'];
+
+console.log(propgrammingLanguages);
+
+var studends = ["Drini", "Edlira", "Rigon", "Omer", "Arianita"]
+
+console.log(s1);
+console.log(s2);
+console.log(s3);
+console.log(s4);
+console.log(s5);
+
+var place = ["Podujeva", "Prishtina", "Peja", "Hamburg", "Berlin", "Ferizaj", "Duseldorf", "Drenica", "Shkup", "Shkoder", "Durrs", "Velipoj", "Ulqin", "Tropoj", "Anwers", "Lipjan" , "Fushkosva", "Madrid" , "Budapest", "Moska"]
+
+console.log(Podujeva);
+console.log(Prishtina);
+console.log(Peja);
+console.log(Hamburg);
+console.log(Berlin);
+console.log(Ferizaj);
+console.log(Duseldorf);
+console.log(s3);
+console.log(s4);
+console.log(s5);
+console.log(s1);
+console.log(s2);
+console.log(s3);
+console.log(s4);
+console.log(s5);
+console.log(s1);
+console.log(s2);
+console.log(s3);
+console.log(s4);
+console.log(s5);
