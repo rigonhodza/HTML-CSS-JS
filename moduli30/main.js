5b13e93efb1904f6844bbb43dc31462ff0337959
rigonhodza <rigon.hoxha10@gmail.com>
2026-03-26 17:44:17 +0100

    ;oug

---
+++ moduli30/main.js
@@ -0,0 +1,7 @@
+var mosha=7
+
+if(mosha<8){
+    document.write("Femija yt nuk mundet me u regjistru sepse esht i vogel")
+}else{
+    document.write("Femija yt mundet vetem prite telefonaten per grupin e tij")
+}
