adf71fa5b9efea5761267bfc508b6cb741ff61a8
rigonhodza <rigon.hoxha10@gmail.com>
2026-05-12 17:46:23 +0200

    moduli36

---
+++ moduli36/main.js
@@ -0,0 +1,12 @@
+// $(window).ready(function(){
+//     alert("It is working")
+// })
+
+// $("#button").click(function(){
+//     console.log("The testing has been successfully")
+// })
+
+$("#button").click(function(){
+    $("div").addClass("clicked")
+
+}).find('span').attr('title', 'Hover over me')
